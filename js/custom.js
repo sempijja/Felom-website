269eb366291f06872d31b281c9371577a40a9835
Ssempijja Charles Charz <ssempijacharz@gmail.com>
2024-12-07 10:26:58 +0300

    Mapping fixes

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(".owl-carousel").owlCarousel({
 
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
+        center: new google.maps.LatLng(0.4595080, 32.6182460),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
